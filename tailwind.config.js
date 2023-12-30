6230a2f3f6deeebf1ac2f63c36d081eac2f43c6a
Betta Raphael <betta@Bettas-MacBook-Pro-2.local>
2023-12-30 09:29:07 +0100

    working on what clients say about us

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      primary: 'Playfair Display',
+      body: 'Work Sans',
+    },
+    extend: {
+      colors: {
+        primary: '#FD620C',
+        secondary: '#1C1D24',
+        tertiary: '#131419',
+        accent: {
+          DEFAULT: '#ac6b34',
+          hover: '#925a2b',
+        },
+        paragraph: '#878e99',
+      },
+    },
+  },
+  plugins: [],
+}
+
